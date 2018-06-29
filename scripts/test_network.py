7c48000bf5f3f807f198d9109a39d3acfa11a1ef
lytlovelife <273453231@qq.com>
2018-06-29 11:18:55 +0800

    1111

---
+++ scripts/test_network.py
@@ -0,0 +1,36 @@
+from appium import webdriver
+
+
+class TestNetwork:
+
+    def setup(self):
+        # server 启动参数
+        desired_caps = dict()
+        # 设备信息
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '5.1'
+        desired_caps['deviceName'] = '192.168.56.101:5555'
+        # app的信息
+        desired_caps['appPackage'] = 'com.android.settings'
+        desired_caps['appActivity'] = '.Settings'
+        # 解决输入中文
+        desired_caps['unicodeKeyboard'] = True
+        desired_caps['resetKeyboard'] = True
+
+        # 声明我们的driver对象
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+    def test_mobile_network_2g(self):
+        self.driver.find_element_by_xpath("//*[contains(@text,'更多')]").click()
+        self.driver.find_element_by_xpath("//*[contains(@text,'移动网络')]").click()
+        self.driver.find_element_by_xpath("//*[contains(@text,'首选网络类型')]").click()
+        self.driver.find_element_by_xpath("//*[contains(@text,'2G')]").click()
+
+    def test_mobile_network_3g(self):
+        self.driver.find_element_by_xpath("//*[contains(@text,'更多')]").click()
+        self.driver.find_element_by_xpath("//*[contains(@text,'移动网络')]").click()
+        self.driver.find_element_by_xpath("//*[contains(@text,'首选网络类型')]").click()
+        self.driver.find_element_by_xpath("//*[contains(@text,'3G')]").click()
+
+    def teardown(self):
+        self.driver.quit()
